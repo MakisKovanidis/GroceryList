d526046b4ea0c44b9cfc6e9d40fa90db99bbe7fd
Makis <makis_kovanidis@hotmail.com>
2019-11-04 18:01:58 +0200

    udemy app

---
+++ app/src/main/java/com/soft/kovanidis/mygrocerylist/Util/Constants.java
@@ -0,0 +1,15 @@
+package com.soft.kovanidis.mygrocerylist.Util;
+
+public class Constants {
+    public static final int BB_VERSION = 1;
+    public static final String DB_NAME = "groceryListDB";
+    public static final String TABLE_NAME = "groceryTBL";
+
+    //table columns
+
+    public static final String KEY_ID ="id";
+    public static final String KEY_GROCERY_ITEM = "grocery_item";
+    public static final String KEY_QTY_NUMBER = "quantity_number";
+    public static final String KEY_DATE_NAME = "date_added";
+
+}
